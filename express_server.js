9aecfdc150c3fa72bcf46d73f6a76bd3f71a73cd
byeong0430 <byeong.kim0430@gmail.com>
2018-06-16 06:15:22 +0000

    edit button's method changed from post to get

--- express_server.js
+++ express_server.js
@@ -76,7 +76,9 @@ app.set('view engine', 'ejs');
 
 // get
 app.get('/', (req, res) => {
-  res.redirect('/urls');
+  const cookieUserId = req.session.user_id;
+  (cookieUserId === undefined) ? res.redirect('/login') : res.redirect('/urls');
+  // res.redirect('/urls');
 });
 
 // login
@@ -84,13 +86,13 @@ app.get('/login', (req, res) => {
   res.render('login_form');
 });
 
-app.get('/register', (req, res) => {
-  res.render('register_form');
-});
+// app.get('/register', (req, res) => {
+//   res.render('register_form');
+// });
 
-app.get('/logout', (req, res) => {
-  res.redirect('/urls');
-})
+// app.get('/logout', (req, res) => {
+//   res.redirect('/urls');
+// })
 
 // render urls_index.ejs and display a table of shortURL and longURL
 app.get('/urls', (req, res) => {
@@ -111,45 +113,45 @@ app.get('/urls', (req, res) => {
   }
 });
 
-// if user id found in cookie, render urls_new.ejs. otherwise redirect to login page
-app.get('/urls/new', (req, res) => {
-  const cookieUserId = req.session.user_id;
-  if (cookieUserId) {
-    res.render('urls_new', { user: users[cookieUserId] });
-  } else {
-    res.redirect('/login');
-  }
-});
+// // if user id found in cookie, render urls_new.ejs. otherwise redirect to login page
+// app.get('/urls/new', (req, res) => {
+//   const cookieUserId = req.session.user_id;
+//   if (cookieUserId) {
+//     res.render('urls_new', { user: users[cookieUserId] });
+//   } else {
+//     res.redirect('/login');
+//   }
+// });
 
-// when shortURL used, redirect to the original URL and store each visit numbers in cookie
-app.get('/u/:shortURL', (req, res) => {
-  const cookieUserId = req.session.user_id;
-  const shortURL = req.params.shortURL;
-  const {address, count} = urlDatabase[shortURL];
+// // when shortURL used, redirect to the original URL and store each visit numbers in cookie
+// app.get('/u/:shortURL', (req, res) => {
+//   const cookieUserId = req.session.user_id;
+//   const shortURL = req.params.shortURL;
+//   const {address, count} = urlDatabase[shortURL];
   
-  // if (!urlDatabase[shortURL]) {
-  //   // entered incorrect shortURL 
-  //   const templateVars = {
-  //     user: users[cookieUserId],
-  //     authMessage: `${shortURL} does not exist!`
-  //   }
-  //   res.render('urls_show', templateVars);
-  //   return;
-  // }
+//   // if (!urlDatabase[shortURL]) {
+//   //   // entered incorrect shortURL 
+//   //   const templateVars = {
+//   //     user: users[cookieUserId],
+//   //     authMessage: `${shortURL} does not exist!`
+//   //   }
+//   //   res.render('urls_show', templateVars);
+//   //   return;
+//   // }
 
-  // check if the current user id is found in count.
-  if(Object.keys(count).indexOf(cookieUserId) === -1){
-    // if not found, add the first count
-    urlDatabase[shortURL].count[cookieUserId] = {
-      visit_count: 1
-    }
-  }else{
-    // if found, add 1
-    urlDatabase[shortURL].count[cookieUserId].visit_count++;
-  }
-  // redirect to the original URL
-  res.redirect(address);
-});
+//   // check if the current user id is found in count.
+//   if(Object.keys(count).indexOf(cookieUserId) === -1){
+//     // if not found, add the first count
+//     urlDatabase[shortURL].count[cookieUserId] = {
+//       visit_count: 1
+//     }
+//   }else{
+//     // if found, add 1
+//     urlDatabase[shortURL].count[cookieUserId].visit_count++;
+//   }
+//   // redirect to the original URL
+//   res.redirect(address);
+// });
 
 // if user is already logged in, render urls_show.ejs. otherwise, redirect to login
 app.get('/urls/:id', (req, res) => {
@@ -159,7 +161,7 @@ app.get('/urls/:id', (req, res) => {
     res.redirect('/login');
     return;
   }
-
+  
   const shortURL = req.params.id;
   if (!urlDatabase[shortURL]) {
     // entered incorrect shortURL 
@@ -173,14 +175,13 @@ app.get('/urls/:id', (req, res) => {
 
   // user already logged in and correct shortURL entered
   if (urlDatabase[shortURL].owner === cookieUserId) {
-    // const visitCount = urlDatabase[shortURL].count[cookieUserId];
+    // owner of shortURL is the current user
+    // count in urlDatabase
     const countField = urlDatabase[shortURL].count;
     let totalVisitCount = 0;
     Object.keys(countField).forEach(user_id => {
       totalVisitCount += countField[user_id].visit_count;
     })
-    
-    // owner of shortURL is the current user
     const templateVars = {
       shortURL: shortURL,
       longURL: urlDatabase[shortURL].address,
@@ -189,8 +190,12 @@ app.get('/urls/:id', (req, res) => {
     };
     res.render('urls_show', templateVars);
   } else {
+    const templateVars = {
+      user: users[cookieUserId],
+      authMessage: `You cannot view details for ${shortURL}`
+    };
     // shortURL doesn't belong to the current user
-    res.render('urls_show', { authMessage: `You cannot view details for ${shortURL}` });
+    res.render('urls_show', templateVars);
   }
 });
 
@@ -228,133 +233,133 @@ app.post('/login', (req, res) => {
   }
 });
 
-app.post('/logout', (req, res) => {
-  // destroy session when logging out
-  req.session = null;
-  res.redirect('/urls');
-});
+// app.post('/logout', (req, res) => {
+//   // destroy session when logging out
+//   req.session = null;
+//   res.redirect('/urls');
+// });
 
 
-app.post('/register', (req, res) => {
-  // receive email and password from request body
-  const { email, password } = req.body;
-  // number of matching records
-  const matchingRecords = Object.values(users).filter(user => user.email === email);
+// app.post('/register', (req, res) => {
+//   // receive email and password from request body
+//   const { email, password } = req.body;
+//   // number of matching records
+//   const matchingRecords = Object.values(users).filter(user => user.email === email);
 
-  // empty email or password
-  if (!email || !password) {
-    res.status(400).render('register_form', { message: 'Registration form incomplete!' });
-    return;
-  }
+//   // empty email or password
+//   if (!email || !password) {
+//     res.status(400).render('register_form', { message: 'Registration form incomplete!' });
+//     return;
+//   }
 
-  if (matchingRecords.length) {
-    // no matching record. add the registration data form into db
-    // make sure password is hashed with 13 salts
-    newKey = generateRandomString(10);
-    users[newKey] = {
-      id: newKey,
-      email: email,
-      password: bcrypt.hashSync(password, 13)
-    };
-    // return back to the log in form and ask user to log in again
-    res.render('login_form', { message: 'Please log in with your new credentials' });
-  } else {
-    // there is a matching record. render the registration form with a message.
-    res.status(400).render('register_form', { message: 'Email already in use' });
-  }
-});
+//   if (matchingRecords.length) {
+//     // no matching record. add the registration data form into db
+//     // make sure password is hashed with 13 salts
+//     newKey = generateRandomString(10);
+//     users[newKey] = {
+//       id: newKey,
+//       email: email,
+//       password: bcrypt.hashSync(password, 13)
+//     };
+//     // return back to the log in form and ask user to log in again
+//     res.render('login_form', { message: 'Please log in with your new credentials' });
+//   } else {
+//     // there is a matching record. render the registration form with a message.
+//     res.status(400).render('register_form', { message: 'Email already in use' });
+//   }
+// });
 
-// create URL button clicked. redirect to /urls/new
-app.post('/urls/new', (req, res) => {
-  res.redirect('/urls/new');
-});
+// // create URL button clicked. redirect to /urls/new
+// app.post('/urls/new', (req, res) => {
+//   res.redirect('/urls/new');
+// });
 
-// new longURL submitted check if it has no error
-app.post('/urls', (req, res) => {
-  // trim white spaces and convert everything to lowercase
-  const longURL = req.body.longURL.toLowerCase().trim();
-  // options for request() function
-  const options = {
-    url: longURL,
-    timeout: 3000
-  };
-  // create object to compile results of getRequestResults()
-  const reqInput = {
-    user: users[req.session.user_id],
-    longURL: longURL,
-    suggestion: ''
-  };
+// // new longURL submitted check if it has no error
+// app.post('/urls', (req, res) => {
+//   // trim white spaces and convert everything to lowercase
+//   const longURL = req.body.longURL.toLowerCase().trim();
+//   // options for request() function
+//   const options = {
+//     url: longURL,
+//     timeout: 3000
+//   };
+//   // create object to compile results of getRequestResults()
+//   const reqInput = {
+//     user: users[req.session.user_id],
+//     longURL: longURL,
+//     suggestion: ''
+//   };
 
-  // send a request to a new URL and receive a response
-  getRequestResults(request, options, reqInput).then(templateVars => {
-    if (templateVars.errName) {
-      // error found. render urls_new with a error message
-      res.render('urls_new', templateVars);
-    } else {
-      // longURL works fine. Update the database
-      const newKey = generateRandomString(6);
-      urlDatabase[newKey] = {
-        id: newKey,
-        address: longURL,
-        owner: req.session.user_id
-      };
-      res.redirect('/urls');
-    }
-  });
-});
+//   // send a request to a new URL and receive a response
+//   getRequestResults(request, options, reqInput).then(templateVars => {
+//     if (templateVars.errName) {
+//       // error found. render urls_new with a error message
+//       res.render('urls_new', templateVars);
+//     } else {
+//       // longURL works fine. Update the database
+//       const newKey = generateRandomString(6);
+//       urlDatabase[newKey] = {
+//         id: newKey,
+//         address: longURL,
+//         owner: req.session.user_id
+//       };
+//       res.redirect('/urls');
+//     }
+//   });
+// });
 
-// go to update page via update button
-app.post('/urls/:id', (req, res) => {
-  const shortURL = req.params.id;
-  const cookieUserId = req.session.user_id;
-  // if visitCount is undefined, set it to 0
-  const visitCount = (req.session[`${shortURL}_visit`] === undefined) ? 0 : req.session[`${shortURL}_visit`];
-  const templateVars = {
-    shortURL: shortURL,
-    longURL: urlDatabase[shortURL].address,
-    user: users[cookieUserId],
-    visitCount: visitCount
-  };
-  res.render('urls_show', templateVars);
-});
-
-
-// put: update
-app.put('/urls/:id', (req, res) => {
-  const {shortURL, newLongURL} = req.body;
-  const options = {
-    url: newLongURL,
-    timeout: 3000
-  };
-  // create object to compile results of getRequestResults()
-  const reqInput = {
-    user: users[req.session.user_id],
-    shortURL: shortURL,
-    longURL: newLongURL,
-    suggestion: ''
-  };
-
-  // send a request to a new URL and receive a response
-  getRequestResults(request, options, reqInput).then(templateVars => {
-    if (templateVars.errName) {
-      // error found in URL. render urls_show.ejs to show error message
-      res.render('urls_show', templateVars);
-    } else {
-      // longURL works fine. Update the database
-      urlDatabase[shortURL].address = newLongURL;
-      res.redirect('/urls');
-    }
-  });
-});
+// // go to update page via update button
+// app.post('/urls/:id', (req, res) => {
+//   const shortURL = req.params.id;
+//   const cookieUserId = req.session.user_id;
+//   // if visitCount is undefined, set it to 0
+//   const visitCount = (req.session[`${shortURL}_visit`] === undefined) ? 0 : req.session[`${shortURL}_visit`];
+//   const templateVars = {
+//     shortURL: shortURL,
+//     longURL: urlDatabase[shortURL].address,
+//     user: users[cookieUserId],
+//     visitCount: visitCount
+//   };
+//   res.render('urls_show', templateVars);
+// });
 
 
-// delete
-// post -> delete by method-override
-app.delete('/urls/:id', (req, res) => {
-  const shortURL = req.params.id;
-  delete urlDatabase[shortURL];
-  res.redirect('/urls');
-});
+// // put: update
+// app.put('/urls/:id', (req, res) => {
+//   const {shortURL, newLongURL} = req.body;
+//   const options = {
+//     url: newLongURL,
+//     timeout: 3000
+//   };
+//   // create object to compile results of getRequestResults()
+//   const reqInput = {
+//     user: users[req.session.user_id],
+//     shortURL: shortURL,
+//     longURL: newLongURL,
+//     suggestion: ''
+//   };
+
+//   // send a request to a new URL and receive a response
+//   getRequestResults(request, options, reqInput).then(templateVars => {
+//     if (templateVars.errName) {
+//       // error found in URL. render urls_show.ejs to show error message
+//       res.render('urls_show', templateVars);
+//     } else {
+//       // longURL works fine. Update the database
+//       urlDatabase[shortURL].address = newLongURL;
+//       res.redirect('/urls');
+//     }
+//   });
+// });
+
+
+// // delete
+// // post -> delete by method-override
+// app.delete('/urls/:id', (req, res) => {
+//   const shortURL = req.params.id;
+//   delete urlDatabase[shortURL];
+//   res.redirect('/urls');
+// });
 
 
 app.listen(port, () => {
